4bd082679441abf422be922d4acdc78fa324df6c
yaobaofeng <11>
2020-05-18 09:43:24 +0800

    状态模式

---
+++ src/main/java/behavioralpattern/statepattern/statemachine/Order.java
@@ -0,0 +1,30 @@
+package behavioralpattern.statepattern.statemachine;
+
+public class Order {
+    private int id;
+    private OrderStatus status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "订单号id=" + id +
+                ", 订单状态status=" + status +
+                '}';
+    }
+}
